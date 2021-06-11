5d4fb8649564156ef6151550e84822bb435cd528
jekoo <pjkwprn@gmail.com>
2021-06-11 13:18:31 +0900

    feat : set axios

---
+++ y-storage/src/api/axios.ts
@@ -0,0 +1,28 @@
+import axios, { AxiosInstance } from 'axios';
+
+const setHeader = () => {
+  axios.defaults.headers.post['Content-Type'] = 'application/json';
+};
+
+export const getAxios = (reqUrl: string, reqParams: Record<string, unknown>= {}): Promise<AxiosInstance> => {
+  setHeader();
+  return axios
+    .create({
+      baseURL: 'http://localhost:3000/',
+      method: 'get',
+      timeout: 10000,
+      params: reqParams,
+    })
+    .request({ url: reqUrl });
+};
+
+export const postAxios = (reqUrl: string, reqData: Record<string, unknown>): Promise<AxiosInstance> => {
+  setHeader();
+  return axios
+    .create({
+      baseURL: 'http://localhost:3000/',
+      method: 'post',
+      timeout: 10000,
+    })
+    .request({ url: reqUrl, data: reqData });
+};
