6cfc3a0c1d13d693490b1004b0ad1580242c68d8
jekoo <pjkwprn@gmail.com>
2021-06-22 19:09:23 +0900

    feat : apply cart alert

--- y-storage/src/component/common/checkIcon.tsx
+++ y-storage/src/component/common/checkIcon.tsx
@@ -6,11 +6,19 @@ import {CartDownload} from '@styled-icons/boxicons-solid/CartDownload'
 const CartIcon = styled(CartDownload)`
   width: 30px;
   color: ${color.blue.lv3};
+  &:hover {
+    transform: scale(1.1);
+    cursor: pointer;
+  }
 `
 
 const CheckIcon: React.FC = () => {
+  const alertHandler = () => {
+    alert("담고 싶은 노래를 체크해 주세요!")
+  }
+
   return (
-    <CartIcon></CartIcon>
+    <CartIcon onClick={() => alertHandler()}></CartIcon>
   )
 }
 
