ee410b7cb02923da8f26a25428d10ab699f1e267
jekoo <pjkwprn@gmail.com>
2021-06-27 18:27:06 +0900

    chore : change ydl option

--- y-server/y-server/routes/file.js
+++ y-server/y-server/routes/file.js
@@ -2,6 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/file', (req, res, next) => {
+  const fs = require('fs');
+  const youtubedl = require('youtube-dl');
+  const options = ['--format=18'];
+  // const options = ['--audio-format FORMAT mp3'];
+  const video = youtubedl(
+    'http://www.youtube.com/watch?v=90AiXO1pAiA',
+    // Optional arguments passed to youtube-dl.
+    options,
+    // Additional options can be given for calling `child_process.execFile()`.
+    { cwd: __dirname }
+  );
+
+  // Will be called when the download starts.
+  video.on('info', function (info) {
+    console.log(info);
+    console.log('Download started');
+    console.log('filename: ' + info._filename);
+    console.log('size: ' + info.size);
+  });
+
+  video.pipe(fs.createWriteStream('myvideo.mp4'));
   const { selectList } = req.body;
   if (selectList) {
     res.status(200).json({ h: 'i' });
