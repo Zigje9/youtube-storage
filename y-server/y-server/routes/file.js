0c660ccdbbb1ca42d50b45aad53fc92eebdf21b8
jekoo <pjkwprn@gmail.com>
2021-06-27 18:12:16 +0900

    chore : set status file router

--- y-server/y-server/routes/file.js
+++ y-server/y-server/routes/file.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/file', (req, res, next) => {
-  console.log(req);
-  res.status(200).json({ h: 'i' });
+  const { selectList } = req.body;
+  if (selectList) {
+    res.status(200).json({ h: 'i' });
+  } else {
+    res.status(400).json({ message: '잘못된 데이터 형식' });
+  }
 });
 
 module.exports = router;
