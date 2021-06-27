a545f6031c85cea80714eb232f1c6ae7c0ae07f5
jekoo <pjkwprn@gmail.com>
2021-06-27 17:16:28 +0900

    docs : read me todoList update

--- y-storage/src/component/common/checkBox.tsx
+++ y-storage/src/component/common/checkBox.tsx
@@ -64,7 +64,6 @@ const SpanCheckBox = styled.span`
 const CheckBox: React.FC<Props> = ({ ...props }: Props) => {
   const selectListHandler = props.selectFunction;
   const checkHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    // selectListHandler(props.vid, e.target.checked)
     selectListHandler(props.videoId, props.videoTitle, e.target.checked);
   };
   return (
