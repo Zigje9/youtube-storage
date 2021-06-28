1a39fee4b2a6300a66139247de5f3d2afe78e915
jekoo <pjkwprn@gmail.com>
2021-06-28 18:50:37 +0900

    feat : create s3 instance

--- y-server/y-server/routes/file.js
+++ y-server/y-server/routes/file.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
+require('dotenv').config();
+
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  region: process.env.AWS_REGION,
+});
 
 router.post('/file', (req, res, next) => {
   const fs = require('fs');
@@ -21,8 +29,63 @@ router.post('/file', (req, res, next) => {
     console.log('filename: ' + info._filename);
     console.log('size: ' + info.size);
   });
+  const temp = () => {
+    video.pipe(fs.createWriteStream('samplevideo.mp3'));
+    return new Promise((resolve, rejects) => {
+      const params = {
+        Bucket: 'y-storage',
+        Key: '세번째.mp3',
+        ACL: 'public-read-write',
+        Body: fs.createReadStream('./samplevideo.mp3'),
+        ContentType: 'audio/mpeg',
+      };
 
-  video.pipe(fs.createWriteStream('myvideo.mp3'));
+      s3.upload(params, (err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(data);
+          // return new Promise((resolve, rejects) => {
+          //   fs.unlink('./myvideo.mp3', () => {
+          //     console.log('deleted ~~~~~~~~');
+          //   });
+          // });
+        }
+      });
+    });
+  };
+  temp();
+  // fs.unlink('./myvideo.mp3', () => {});
+  // video.pipe(fs.createWriteStream('myvideo.mp3')).then(() => {
+  //   const params = {
+  //     Bucket: 'y-storage',
+  //     Key: 'tempvideo.mp3',
+  //     ACL: 'public-read-write',
+  //     Body: fs.createReadStream('./myvideo.mp3'),
+  //     ContentType: 'audio/mpeg',
+  //   };
+
+  //   s3.upload(params, () => {});
+  // });
+
+  // const params = {
+  //   Bucket: 'y-storage',
+  //   Key: 'tempvideo.mp3',
+  //   ACL: 'public-read-write',
+  //   // Body: fs.createReadStream('./routes/test.txt'),
+  //   Body: video.pipe(createWriteStream('myvideo.mp3')),
+  //   ContentType: 'audio/mpeg',
+  // };
+
+  // s3.upload(params, () => {});
+  // s3.getObject(params, (err, data) => {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     console.log(data);
+  //   }
+  // });
+
   const { selectList } = req.body;
   if (selectList) {
     res.status(200).json({ h: 'i' });
