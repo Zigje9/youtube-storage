2b1d8c8aec4977dc107d99e574fd13388456cf65
jekoo <pjkwprn@gmail.com>
2021-06-29 23:55:59 +0900

    feat : get object list from s3

--- y-storage/src/view/storageView.tsx
+++ y-storage/src/view/storageView.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
+import 'dotenv/config';
+
+import AWS from 'aws-sdk';
+const s3 = new AWS.S3({
+  accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY,
+  secretAccessKey: process.env.REACT_APP_AWS_SECRET_KEY,
+  region: process.env.REACT_APP_AWS_REGION,
+});
 
 const StorageView: React.FC = () => {
+  const params: any = {
+    Bucket: process.env.REACT_APP_AWS_BUCKET,
+    MaxKeys: 100,
+  };
+  s3.listObjectsV2(params, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    }
+  });
   return <h1>storage View</h1>;
 };
 
