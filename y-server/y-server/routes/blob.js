3a947d07f19d06285291a5e38200ec50cbc0542e
jekoo <pjkwprn@gmail.com>
2021-07-06 02:54:27 +0900

    chore : delete console

--- y-server/y-server/routes/blob.js
+++ y-server/y-server/routes/blob.js
@@ -6,7 +6,6 @@ router.post('/blob', async (req, res, next) => {
   const { fileName } = req.body;
   try {
     const data = await getBlobObject(fileName);
-    console.log(data);
     res.status(200).json(data);
   } catch (error) {
     res.status(400).json({ message: '요청 실패' });
